563f9ab62cc36fc24bf56aacd0e6c9a29a9632bb
muhzulzidan <mzulzidan@gmail.com>
2023-03-08 23:37:30 +0800

    finish the design intro, register, login

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -1,99 +1,230 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:http/http.dart' as http;
 
-
-final TextEditingController _emailController = TextEditingController();
-final TextEditingController _nameController = TextEditingController();
-final TextEditingController _passwordController = TextEditingController();
-final TextEditingController _phoneNumberController = TextEditingController();
+import '../../controller/auth/auth_state.dart';
 
 class RegisterScreen extends StatefulWidget {
+  // final AuthService _authService = AuthService();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
 
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: HexColor("#F2F2F2"),
+        // foregroundColor: HexColor("#F2F2F2"),
+        // title: const Text('Login'),
+      ),
       body: Padding(
         padding: EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Register',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: 'Name',
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Daftar',
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800),
+                      textAlign: TextAlign.left,
+                    ),
                   ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
+                  SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        labelStyle:
+                            TextStyle(fontSize: 12, color: Colors.black54),
+                        labelText: 'Name',
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _phoneNumberController,
-              decoration: const InputDecoration(
-                labelText: 'Phone Number',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
+                  SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        labelStyle:
+                            TextStyle(fontSize: 12, color: Colors.black54),
+                        labelText: 'E-Mail',
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: _phoneNumberController,
+                      decoration: const InputDecoration(
+                        labelStyle:
+                            TextStyle(fontSize: 12, color: Colors.black54),
+                        labelText: 'No. Handphone',
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        labelStyle:
+                            TextStyle(fontSize: 12, color: Colors.black54),
+                        labelText: 'Kata Sandi',
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () => {},
+                        style: ElevatedButton.styleFrom(
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          minimumSize: Size.zero,
+                          padding: EdgeInsets.zero,
+                          backgroundColor: Colors.transparent,
+                        ),
+                        child: Text(
+                          "Lupa kata sandi ?",
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 13),
+                        backgroundColor: Colors.amber,
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                      onPressed: _isLoading ? null : register,
+                      child: _isLoading
+                          ? const CircularProgressIndicator()
+                          : const Text(
+                              'Daftar',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 17),
+                            ),
+                    ),
+                  ),
+                ],
               ),
-              keyboardType: TextInputType.phone,
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-               onPressed: () {
-                register(context);
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-                backgroundColor: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                elevation: 6,
-              ),
-              child: Text(
-                'Register',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.12,
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                // crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text("Tidak punya akun?"),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterScreen()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        minimumSize: Size.zero,
+                        padding: EdgeInsets.zero,
+                        backgroundColor: Colors.transparent,
+                      ),
+                      child: Text("Daftar"))
+                ],
               ),
             ),
           ],
@@ -101,61 +232,68 @@ class _RegisterScreenState extends State<RegisterScreen> {
       ),
     );
   }
-}
 
-void register(BuildContext context) async {
-  final String name = _nameController.text.trim();
-  final String phoneNumber = _phoneNumberController.text.trim();
-  final String email = _emailController.text.trim();
-  final String password = _passwordController.text.trim();
+  Future<void> register() async {
+    setState(() {
+      _isLoading = true;
+    });
 
-  // Perform validation on the input fields
-  if (name.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Please enter your name')),
-    );
-    return;
-  }
+    final String name = _nameController.text.trim();
+    final String phoneNumber = _phoneNumberController.text.trim();
+    final String email = _emailController.text.trim();
+    final String password = _passwordController.text.trim();
 
-  if (phoneNumber.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Please enter your phone number')),
-    );
-    return;
-  }
+    // final result = await _authService.login(email, password);
 
-  if (email.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Please enter your email')),
-    );
-    return;
-  }
+    // Perform validation on the input fields
+    if (name.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter your name')),
+      );
+      return;
+    }
 
-  if (password.isEmpty) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Please enter your password')),
-    );
-    return;
-  }
+    if (phoneNumber.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter your phone number')),
+      );
+      return;
+    }
 
-  // Send the registration data to your backend server
-  // and wait for the response
-  final response = await http
-      .post(Uri.parse('https://mobiltravelapp.shym2501.repl.co/api/register'), body: {
-    'name': name,
-    'phone_number': phoneNumber,
-    'email': email,
-    'password': password,
-  });
+    if (email.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter your email')),
+      );
+      return;
+    }
 
-  // Check the response status code
-  if (response.statusCode == 200) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Registration successful')),
-    );
-  } else {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Registration failed')),
-    );
+    if (password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter your password')),
+      );
+      return;
+    }
+
+    // Send the registration data to your backend server
+    // and wait for the response
+    final response = await http.post(
+        Uri.parse('https://mobiltravelapp.shym2501.repl.co/api/register'),
+        body: {
+          'name': name,
+          'phone_number': phoneNumber,
+          'email': email,
+          'password': password,
+        });
+
+    // Check the response status code
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Registration successful')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Registration failed')),
+      );
+    }
   }
 }
