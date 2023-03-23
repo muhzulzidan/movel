0b70ce0739e80ccf1339d7fe96026e775a882ac2
muhzulzidan <mzulzidan@gmail.com>
2023-03-23 16:40:33 +0800

    update navigation bar at home and add pesanan page

--- lib/screens/inbox.dart
+++ lib/screens/inbox.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home/home.dart';
+
 class InboxScreen extends StatefulWidget {
   @override
   _InboxScreenState createState() => _InboxScreenState();
@@ -9,6 +11,16 @@ class _InboxScreenState extends State<InboxScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            "Kotak Masuk",
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )),
       body: Container(
         child: Center(
           child: Text('This is the inbox screen'),
