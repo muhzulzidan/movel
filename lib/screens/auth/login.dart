d7ecbf2985ea79fa9faddb9ad78d7d3dc950c4f5
muhzulzidan <mzulzidan@gmail.com>
2023-03-30 14:47:11 +0800

    update profile, with some screens like change password, pengaturan, riwayat, alamat, pusat bantuan,

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -148,7 +148,6 @@ class _LoginScreenState extends State<LoginScreen> {
                           ),
                           child: Text(
                             "Lupa kata sandi ?",
-                            
                           ),
                         ),
                       ],
