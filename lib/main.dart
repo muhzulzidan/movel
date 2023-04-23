4b3324e2018581a560411c72ae41bcd977cbb384
muhzulzidan <mzulzidan@gmail.com>
2023-04-23 12:42:28 +0800

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget {
         ),
         // home: LoginScreen(),
         debugShowCheckedModeBanner: false,
-        initialRoute: '/',
+        initialRoute: '/home',
         // home: isLoggedIn ? IntroScreen() : MyHomePage(),
         routes: {
           '/': (context) => IntroScreen(),
